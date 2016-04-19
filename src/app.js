4965c80d99317dd21a36f73fb91236e7abab78d0
ewgenius <ewgeniux@gmail.com>
2016-04-19 22:53:37 +0300

    pubnub integration

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import {
   THREE
 } from 'three'
+import Pubnub from 'pubnub'
 
 const Scene = THREE.Scene
 const PerspectiveCamera = THREE.PerspectiveCamera
@@ -20,6 +21,31 @@ class CubeMesh extends Mesh {
 
 class Application {
   start() {
+    this.pubnub = new Pubnub({
+      publish_key: 'pub-c-e0b788e5-deaa-44f9-8f6e-a5e97fe3b6cf',
+      subscribe_key: 'sub-c-c825c9b2-0666-11e6-a5b5-0619f8945a4f'
+    })
+    console.log('Subscribing..');
+    this.pubnub.subscribe({
+      channel: 'hello_world',
+      message: (message, envelope, channelOrGroup, time, channel) => console.log(`
+        Message Received.
+        Channel or Group: ${JSON.stringify(channelOrGroup)}
+        Channel: ${JSON.stringify(channel)}
+        Message: ${JSON.stringify(message)}
+        Time: ${time}
+        Raw Envelope: ${JSON.stringify(envelope)}
+        `),
+      connect: () => {
+        console.log(`Since we're publishing on subscribe connectEvent, we're sure we'll receive the following publish.`)
+        this.pubnub.publish({
+          channel: 'hello_world',
+          message: 'Hello from PubNub Docs!',
+          callback: m => console.log(m)
+        })
+      }
+    })
+
     this.camera = new PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000)
     this.camera.position.x = 400
     this.camera.position.z = 400
